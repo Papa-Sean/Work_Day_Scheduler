873fff1dde90356a9f2b1799894a2cdc9848da56
Papa-Sean <smsugrue@gmail.com>
2020-10-13 17:31:37 -0400

    added time/date linked display and made localstorage

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
     
+    //for current day and time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss: a"));
+
+    $(".saveBtn")on("click", function () {
+        //when the save button is clicked (this) will be logged
+        console.log(this);
+        //what is logged will now be displayed in the description
+        var text = $(this).siblings(".description").val();
+        //telling the page which text area to find based on the id associated with it
+        var time = $(this).parent().attr("id");
+        //create local storage to be pulled up
+        localStorage.setItem(time, text);
+    })
+
+    //when page refreshes, it needs to ask for info from local storage, here is a loop that will do so
+    
 })
